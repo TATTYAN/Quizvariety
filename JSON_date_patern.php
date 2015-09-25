6489bfcae47b41e94d2463b08c8ca796ea4e2af8
高橋良成 <zexel8716@gmail.com>
2015-09-26 03:38:52 +0900

    JSON_date_patern.php追加

---
+++ JSON_date_patern.php
@@ -0,0 +1,544 @@
+<?php
+mb_convert_encoding($string, 'sjis-win', 'utf8');
+ // $array = array("answer"=>"アシアル","title"=>"http://a1.zeroprm.com/b31_c467/Tojson.php");
+$arr = array( 
+    array(
+        "answer" => "2",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "女性に恋話をする切り出し方として、適切なものは？",
+        "sel1" => "「過去に付き合った人数は？」と、率直に聞く",
+         "sel2" => "自分が５分以上話せるネタのものを用意しておく",
+        "sel3" => "常に、他の質問を用意し、切り替えやすくする",
+         "detail" => "質問だけ用意しても、話は広がらない。他の選択肢も半分正解ではあるが、鉄則として、使える質問というのは自分が話せるネタがある質問だけ。"
+    ),
+    array( 
+       "answer" => "建学の精神とキリスト教－2",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/02/102001002.html",
+        "sel1" => "春学期  今出川",
+        "sel2" => "火3",
+        "sel3" => "2",
+         "detail" => "★★★★☆"
+    ),
+     array( 
+       "answer" => "科学史・科学論1-5",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/69/169311005.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "火4",
+        "sel3" => "2",
+         "detail" => "★★★☆☆"
+    ),
+    array( 
+       "answer" => "線形代数学1-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/60/160003001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "水4",
+        "sel3" => "2",
+         "detail" => "★★☆☆☆"
+    ),
+    array( 
+       "answer" => "解析学1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/66/166074000.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "水3",
+        "sel3" => "2",
+         "detail" => "★☆☆☆☆"
+    ),
+    array( 
+       "answer" => "数理統計学",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/G1/1G1061000.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "月1",
+        "sel3" => "2",
+         "detail" => "★★★★☆"
+    ),
+    array( 
+       "answer" => "物質の科学1-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/69/169305001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "木3",
+        "sel3" => "2",
+         "detail" => "★★☆☆☆"
+    ),
+    array( 
+       "answer" => "スポーツ・パフォーマンス1-101",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/88/188072101.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "木1",
+        "sel3" => "1",
+         "detail" => "★★★★☆"
+    ),
+    array( 
+       "answer" => "データベースシステム",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/67/167110000.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火2",
+        "sel3" => "2",
+         "detail" => "★★★★★"
+    ),
+    array( 
+       "answer" => "スポーツ・パフォーマンス1-362",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/88/188072362.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "金3",
+        "sel3" => "1",
+         "detail" => "★★★★☆"
+    ),
+    array( 
+       "answer" => "イングリッシュ・セミナー1-5",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195031005.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "火1",
+        "sel3" => "1",
+         "detail" => "★★★★☆"
+    ),
+    array( 
+       "answer" => "イングリッシュ・ワークショップ1-109",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195041109.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "水４",
+        "sel3" => "1",
+         "detail" => "★☆☆☆☆"
+    ),
+    array( 
+       "answer" => "イングリッシュ・ワークショップ1-106",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195041106.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "金1",
+        "sel3" => "1",
+         "detail" => "★★★★☆"
+    ),
+    array( 
+       "answer" => "イングリッシュ・セミナー105",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195031105.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "水2",
+        "sel3" => "1",
+         "detail" => "★★★☆☆"
+    ),
+    array( 
+       "answer" => "物質の科学1-2",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/69/169305002.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "金4",
+        "sel3" => "2",
+         "detail" => "☆☆☆☆☆"
+    ),
+    array( 
+       "answer" => "シミュレーション基礎演習",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/G1/1G1070000.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "木5",
+        "sel3" => "2",
+         "detail" => "★★★★☆"
+    ),
+    array( 
+       "answer" => "建学の精神とキリスト教-3",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/02/102001003.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "月3",
+        "sel3" => "2",
+         "detail" => "★★★★★"
+    ),
+    array( 
+       "answer" => "論理表現",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/66/166076000.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火5",
+        "sel3" => "2",
+         "detail" => "★★★☆☆"
+    ),
+    array( 
+       "answer" => "物質の科学1-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/69/169305001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "木3",
+        "sel3" => "2",
+         "detail" => "★★☆☆☆"
+    ), array( 
+       "answer" => "イングリッシュ・ワークショップ1-102",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195041102.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火2",
+        "sel3" => "1"
+    ),
+    array( 
+       "answer" => "イングリッシュ・ワークショップ1-103",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195041103.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "火4",
+        "sel3" => "1"
+    ),
+    array( 
+       "answer" => "イングリッシュ・ワークショップ1-104",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195041104.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火4",
+        "sel3" => "1"
+    ),
+    array( 
+       "answer" => "イングリッシュ・ワークショップ1-105",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195041105.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "木3",
+        "sel3" => "1"
+    ),
+    array( 
+       "answer" => "イングリッシュ・ワークショップ1-107",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195041107.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "金1",
+        "sel3" => "1"
+    ),
+    array( 
+       "answer" => "イングリッシュ・ワークショップ1-108",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195041108.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "金1",
+        "sel3" => "1"
+    ),
+    array( 
+       "answer" => "ドイツ語入門1-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195111001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "火2木1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "ドイツ語入門1-11",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195111011.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "ドイツ語入門1-12",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195111012.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "ドイツ語入門2-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195112001.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火2木1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "ドイツ語入門2-11",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195112011.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "ドイツ語入門2-12",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195112012.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "ロシア語入門1-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195511001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "ロシア語入門1-2",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195511002.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "ロシア語入門2-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195512001.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "ロシア語入門2-2",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195512002.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "フランス語入門1-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195211001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "フランス語入門2-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195212001.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "スペイン語入門1-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195411001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "スペイン語入門2-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195412001.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "中国語入門1-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195311001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "中国語入門2-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195312001.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "コリア語入門1-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195811001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "コリア語入門2-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/95/195812001.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火4土1",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "キリスト教と人間1-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/80/180012001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "火2",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "キリスト教と人間1-51",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/80/180012051.html",
+        "sel1" => "春学期  今出川",
+        "sel2" => "火2",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "  キリスト教と人間2-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/80/180013001.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火2",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "キリスト教と人間2-51",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/80/180013051.html",
+        "sel1" => "秋学期  今出川",
+        "sel2" => "火2",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "キリスト教会と現代文化1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/80/180016001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "木2",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "キリスト教会と現代文化51",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/80/180016051.html",
+        "sel1" => "春学期  今出川",
+        "sel2" => "木2",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "キャリア開発と学生生活1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/80/180051001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "月2",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "キャリア開発と学生生活51",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/80/180051051.html",
+        "sel1" => "春学期  今出川",
+        "sel2" => "火2",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "数学1-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/69/169303001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "金2",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "数学1-2",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/69/169303002.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "木2",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "データサイエンス1-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/lectureWebResult.php",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "木2",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "データサイエンス入門-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/76/176511001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "月3",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "データサイエン入門-2",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/76/176511002.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "木4",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "地球と宇宙の科学1-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/69/169307001.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "金4",
+        "sel3" => "2"
+    ),
+    array( 
+       "answer" => "地球と宇宙の科学2-51",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/69/169308051.html",
+        "sel1" => "秋学期  今出川",
+        "sel2" => "金4",
+        "sel3" => "2"
+    ),
+     array( 
+       "answer" => "生命の科学2-1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/69/169310001.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火3",
+        "sel3" => "2"
+    ),
+      array( 
+       "answer" => "生命の科学2-2",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/69/169310002.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "火4",
+        "sel3" => "2"
+    ),
+      array( 
+       "answer" => "宗教学1",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/01/101321000.html",
+        "sel1" => "秋学期  今出川",
+        "sel2" => "金4",
+        "sel3" => "2"
+    ),
+      array( 
+       "answer" => "宗教学15",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/01/101335000.html",
+        "sel1" => "春学期  今出川",
+        "sel2" => "火5",
+        "sel3" => "2"
+    ),
+      array( 
+       "answer" => "発達心理学",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/13/113502000.html",
+        "sel1" => "春学期  京田辺",
+        "sel2" => "水4",
+        "sel3" => "2"
+    ),
+      array( 
+       "answer" => "心理学史",
+        //"title"  => "http://www.asial.co.jp/" 
+        "title"  => "https://syllabus.doshisha.ac.jp/html/2015/13/113051000.html",
+        "sel1" => "秋学期  京田辺",
+        "sel2" => "木1",
+        "sel3" => "2"
+    )
+    
+
+
+);
+ $json = json_encode($arr);
+
+ echo $json;
+
+
+?>
